6ab5e42e4d36b3f9cb191ff345ca05ebde370f2a
saisatyateja <saisatyateja18@gmail.com>
2026-09-08 02:32:42 +0530

    modify rolloutUndo.groovy

--- vars/rolloutUndo.groovy
+++ vars/rolloutUndo.groovy
@@ -1,12 +1,22 @@
 def call(Map config) {
     sh """
-        kubectl rollout undo \
-            deployment/${config.serviceName} \
-            -n ${config.namespace}
+        REVISION_COUNT= \$(kubectl rollout history deployment/${config.serviceName} \
+            -n ${config.namespace} | awk 'NR > 2 {count++} END {print count}')
 
-        kubectl rollout status \
-            deployment/${config.serviceName} \
-            -n ${config.namespace} \
-            --timeout=5m
+        if [ "\$REVISION_COUNT" -ge 2 ]; then
+
+            echo "Previous rollout found. Rolling back..."
+
+            kubectl rollout undo deployment/${config.serviceName} \
+                -n ${config.namespace}
+
+            kubectl rollout status deployment/${config.serviceName} \
+                -n ${config.namespace} \
+                --timeout=5m
+
+        else
+            echo "No previous rollout available. Skipping rollback."
+        fi
+        
     """
 }
