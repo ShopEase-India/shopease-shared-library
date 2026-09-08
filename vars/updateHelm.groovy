bc10a06d677f890f99acb12309929ba91d2c168b
saisatyateja <saisatyateja18@gmail.com>
2026-09-09 02:50:23 +0530

    modify updateHelm.groovy for git push on helm

--- vars/updateHelm.groovy
+++ vars/updateHelm.groovy
@@ -1,23 +1,24 @@
 def call(Map config) {
 
     dir('shopease-helm') {
-
-        sh """
-            set -e
-
-            echo "Updating ${config.serviceName} image..."
-
-            sed -i 's|tag:.*|tag: "${config.tag}"|' \
-                ${config.serviceName}/values.yaml
-
-            git config user.name "Jenkins"
-            git config user.email "jenkins@shopease.com"
-
-            git add ${config.serviceName}/values.yaml
-
-            git commit -m "Deploy ${config.serviceName}:${config.tag}" || true
-
-            git push origin main
-        """
+        sshagent(['github-helm-read-write']) {
+            sh """
+                set -e
+    
+                echo "Updating ${config.serviceName} image..."
+    
+                sed -i 's|tag:.*|tag: "${config.tag}"|' \
+                    ${config.serviceName}/values.yaml
+    
+                git config user.name "Jenkins"
+                git config user.email "jenkins@shopease.com"
+    
+                git add ${config.serviceName}/values.yaml
+    
+                git commit -m "Deploy ${config.serviceName}:${config.tag}" || true
+    
+                git push origin main
+            """
+        }
     }
 }
