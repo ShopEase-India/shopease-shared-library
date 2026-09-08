69e12500d4f31344272d3d0043a91eaf8d58c0fa
saisatyateja <saisatyateja18@gmail.com>
2026-09-09 02:27:04 +0530

    modify deployService.groovy for argo cd

--- vars/deployService.groovy
+++ vars/deployService.groovy
@@ -1,27 +1,24 @@
 def call(Map config) {
 
-
     try {
 
         sh """
             set -e
 
-            aws eks update-kubeconfig \
-                --name ${config.clusterName} \
+            aws eks update-kubeconfig \\
+                --name ${config.clusterName} \\
                 --region ${config.region}
-
-            helm upgrade --install ${config.serviceName} \
-                ./shopease-helm/${config.serviceName} \
-                -n ${config.namespace} \
-                --create-namespace \
-                --set image.repository=${config.image} \
-                --set image.tag=${config.tag}
-
-            kubectl rollout status \
-                deployment/${config.serviceName} \
-                -n ${config.namespace} \
-                --timeout=5m
         """
+
+        updateHelm(
+                serviceName: config.serviceName,
+                image: config.image,
+                tag: config.tag,
+                helmRepo: config.helmRepo
+        )
+
+        echo "Helm repository updated successfully."
+        echo "Argo CD will deploy ${config.serviceName}:${config.tag}"
 
         healthCheck(
                 serviceName: config.serviceName,
@@ -30,13 +27,7 @@ def call(Map config) {
 
     } catch (Exception e) {
 
-        echo "Deployment or health check failed."
-        echo "Rolling back ${config.serviceName}..."
-
-        rolloutUndo(
-                serviceName: config.serviceName,
-                namespace: config.namespace
-        )
+        echo "Deployment configuration update failed."
 
         throw e
     }
